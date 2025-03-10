aa34e5adb44ee6485303495793bca18985f6ef5c
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-10 10:11:06 -0300

    action

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,31 +3,37 @@ import { Button } from "../components/Button";
 import { useState } from "react";
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    
+
+  function onAction(formData: FormData) {
+    alert(formData.get("email"))
+    alert(formData.get("password"))
   }
   return (
-    <form onSubmit={onSubmit} className="w-full flex flex-col  gap-4">
+    <form action={onAction} className="w-full flex flex-col  gap-4">
       <Input
+        name="email"
         required
         legend="E - mail"
         placeholder="Digite seu E-mail"
-        onChange={(e) => setEmail(e.target.value)}
       />
       <Input
+        name="password"
         required
         type="password"
         legend="Senha"
         placeholder="Digite sua senha"
-        onChange={(e) => setPassword(e.target.value)}
       />
-      <Button isLoading={isLoading} type="submit">Entrar</Button>
+      <Button isLoading={isLoading} type="submit">
+        Entrar
+      </Button>
 
-      <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar Conta</a>
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar Conta
+      </a>
     </form>
   );
 }
